724c803d661e5397cce072768e054f822d7c792c
darberall <darbycristine@gmail.com>
2026-03-07 13:00:31 -0800

    algaeblock now can be bonemealed and grow on randomticks

--- src/main/java/net/loadingsignal/algaemod/block/custom/AlgaeBlock.java
+++ src/main/java/net/loadingsignal/algaemod/block/custom/AlgaeBlock.java
@@ -2,19 +2,17 @@ package net.loadingsignal.algaemod.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.monster.Ravager;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BonemealableBlock;
-import net.minecraft.world.level.block.IceBlock;
 import net.minecraft.world.level.block.WaterlilyBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -23,33 +21,16 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class AlgaeBlock extends WaterlilyBlock implements BonemealableBlock {
+
+
+    //setting up Age property and get methods
     public static final int MAX_AGE = 4;
-    //public static final IntegerProperty AGE = IntegerProperty.create("age", 0, MAX_AGE);
+
     public static final IntegerProperty AGE = BlockStateProperties.AGE_4;
-
-    public AlgaeBlock(BlockBehaviour.Properties pProperties) {
-        super(pProperties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(this.getAgeProperty(), Integer.valueOf(0)));
-       }
-
-    @Override
-    protected boolean mayPlaceOn(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
-        FluidState $$3 = pLevel.getFluidState(pPos);
-        FluidState $$4 = pLevel.getFluidState(pPos.above());
-        return ($$3.getType() == Fluids.WATER) && $$4.getType() == Fluids.EMPTY;
-    }
-    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
-        FluidState blockBelow = level.getFluidState(pos.below());
-        boolean hasLight = level.getRawBrightness(pos, 0) >= 8 || level.canSeeSky(pos);
-        boolean onWater = blockBelow.getType() == Fluids.WATER && blockBelow.isSource();
-        return hasLight && onWater;
-    }
 
     protected IntegerProperty getAgeProperty() {
         return AGE;
@@ -59,19 +40,133 @@ public class AlgaeBlock extends WaterlilyBlock implements BonemealableBlock {
         return MAX_AGE;
     }
 
-    public int getAge(BlockState pState) {
-        return pState.getValue(this.getAgeProperty());
+    public int getAge(BlockState state) {
+        return state.getValue(this.getAgeProperty());
+    }
+
+    public BlockState getStateForAge(int age) {
+        return this.defaultBlockState().setValue(this.getAgeProperty(), Integer.valueOf(age));
+    }
+
+    public final boolean isMaxAge(BlockState state) {
+        return this.getAge(state) >= this.getMaxAge();
+    }
+
+    public AlgaeBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(this.getAgeProperty(), Integer.valueOf(0)));
+       }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
     }
 
 
 
-    public BlockState getStateForAge(int pAge) {
-        return this.defaultBlockState().setValue(this.getAgeProperty(), Integer.valueOf(pAge));
+    //voxel shape and collision shape
+    private static final VoxelShape SHAPE = Block.box(0,0,0,16,1,16);
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+    protected static final VoxelShape COLLISION_SHAPE = Block.box(1.0D, 0.0D, 1.0D, 15.0D, 1.4D, 15.0D);
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return COLLISION_SHAPE;
     }
 
-    public final boolean isMaxAge(BlockState pState) {
-        return this.getAge(pState) >= this.getMaxAge();
+    //block rules-- placing on water, surviving on water with light, boats break it.
+            //may place on water with air above it
+    @Override
+    protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
+        FluidState block = level.getFluidState(pos);
+        FluidState blockAbove = level.getFluidState(pos.above());
+        return (block.getType() == Fluids.WATER) && blockAbove.getType() == Fluids.EMPTY;
     }
+            //can survive if water is source block and can see sky or has light.
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        FluidState blockBelow = level.getFluidState(pos.below());
+        boolean hasLight = level.getRawBrightness(pos, 0) >= 8 || level.canSeeSky(pos);
+        boolean onWater = blockBelow.getType() == Fluids.WATER && blockBelow.isSource();
+        return hasLight && onWater;
+    }
+
+                //boats break algae when they run into it. (voxel collision shape has to be like it is now so that boats can enter the block).
+    @Override
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+        super.entityInside(state, level, pos, entity);
+
+        if (level instanceof ServerLevel && entity instanceof Boat) {
+            level.destroyBlock(pos, true);
+        }
+    }
+
+    //bonemeal functionality
+
+    @Override
+    public boolean isValidBonemealTarget(LevelReader levelReader, BlockPos bpos, BlockState state, boolean b) {
+        return !this.isMaxAge(state);
+    }
+
+    @Override
+    public boolean isBonemealSuccess(Level level, RandomSource randomSource, BlockPos pos, BlockState sState) {
+        return true;
+    }
+
+    @Override
+    public void performBonemeal(ServerLevel serverLevel, RandomSource randomSource, BlockPos pos, BlockState state) {
+
+        this.growCrops(serverLevel, pos, state);
+        /*int currentLevel = this.getAge(blockState);
+
+        // Cycle the value (0-5), then loop back to 0
+        int nextLevel = (currentLevel >= MAX_AGE) ? 0 : currentLevel + 1;
+
+        // Update the block state in the world
+        serverLevel.setBlock(blockPos, blockState.setValue(AGE, nextLevel), 3);
+        */
+
+    }
+
+    protected int getBonemealAgeIncrease(Level pLevel) {
+        return Mth.nextInt(pLevel.random, 2, 5);
+    }
+
+
+    //growing on random ticks
+
+     public boolean isRandomlyTicking(BlockState state) {
+              return !this.isMaxAge(state);
+          }
+      public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+              if (!level.isAreaLoaded(pos, 1))
+                  return; // Forge: prevent loading unloaded chunks when checking neighbor's light
+              if (level.getRawBrightness(pos, 0) >= 9) {
+                  int i = this.getAge(state);
+
+                  if (i < this.getMaxAge()) {
+                      if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(level, pos, state, random.nextInt((int) (25.0F / 10) + 1) == 0)) {
+                          level.setBlock(pos, this.getStateForAge(i + 1), 2);
+                          net.minecraftforge.common.ForgeHooks.onCropsGrowPost(level, pos, state);
+                      }
+                 }
+              }
+          }
+
+
+    public void growCrops(Level level, BlockPos pos, BlockState state) {
+        int i = this.getAge(state) + this.getBonemealAgeIncrease(level);
+        int j = this.getMaxAge();
+        if (i > j) {
+            i = j;
+        }
+
+        level.setBlock(pos, this.getStateForAge(i), 2);
+    }
+
 
 
 
@@ -94,55 +189,6 @@ public class AlgaeBlock extends WaterlilyBlock implements BonemealableBlock {
 */
 
 
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AGE);
-    }
 
-    private static final VoxelShape SHAPE = Block.box(0,0,0,16,1,16);
-
-    @Override
-    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return SHAPE;
-    }
-    protected static final VoxelShape COLLISION_SHAPE = Block.box(1.0D, 0.0D, 1.0D, 15.0D, 1.4D, 15.0D);
-
-    @Override
-    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return COLLISION_SHAPE;
-    }
-
-
-
-    @Override
-    public boolean isValidBonemealTarget(LevelReader levelReader, BlockPos blockPos, BlockState blockState, boolean b) {
-        return !this.isMaxAge(blockState);
-    }
-
-    @Override
-    public boolean isBonemealSuccess(Level level, RandomSource randomSource, BlockPos blockPos, BlockState blockState) {
-        return true;
-    }
-
-    @Override
-    public void performBonemeal(ServerLevel serverLevel, RandomSource randomSource, BlockPos blockPos, BlockState blockState) {
-        int currentLevel = this.getAge(blockState);
-
-        // Cycle the value (0-5), then loop back to 0
-        int nextLevel = (currentLevel >= MAX_AGE) ? 0 : currentLevel + 1;
-
-        // Update the block state in the world
-        serverLevel.setBlock(blockPos, blockState.setValue(AGE, nextLevel), 3);
-    }
-
-    @Override
-    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
-        super.entityInside(state, level, pos, entity);
-
-        // Check if the entity is a boat and if we are on the server
-        if (level instanceof ServerLevel && entity instanceof Boat) {
-            level.destroyBlock(pos, true);
-        }
-    }
 
 }
